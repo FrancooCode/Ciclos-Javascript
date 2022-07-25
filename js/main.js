474f6622b8757d0f7759052dd727f7893f59bcf0
Franco <francoenina79@gmail.com>
2022-07-25 19:29:08 -0300

    ciclos javascript

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+//Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces ingresada.
+
+let ingresarNumero = parseInt(prompt("ingresar numero"))
+    for (let i=1; i <= ingresarNumero; i++){
+        console.log( " hola \n") 
+    }
