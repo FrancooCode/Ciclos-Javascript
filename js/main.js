ec148b5bf3b13dced936a4bb9c03ac779a043508
Franco <francoenina79@gmail.com>
2022-07-29 19:13:17 -0300

    funciones

--- js/main.js
+++ js/main.js
@@ -5,3 +5,40 @@ let ingresarNumero = parseInt(prompt("ingresar numero"))
     for (let i=1; i <= ingresarNumero; i++){
         console.log( " hola \n") 
     }
+
+
+
+    
+
+    //Calcular pagos en cuotas sobre un monto determinado | CLASE 4
+
+function calcularCuota (precioTotal , cantCuotas){
+    
+    switch (cantCuotas){
+        case 3:
+            alert(precioTotal / cantCuotas);
+        break;
+        case 6:
+            alert(precioTotal / cantCuotas);
+        break;
+        case 12:
+            alert(precioTotal / cantCuotas);
+        break;
+        default:
+            alert("No disponemos esa cantidad de cuotas");
+        break;          
+    }
+}
+
+let seguir;
+
+do{
+    const precioTotal = Number(prompt("Ingresa el precio total "))
+
+    const cantCuotas = Number(prompt("En cuantas cuotas deseas pagar? \n3 Cuotas \n6 Cuotas \n12 Cuotas"))
+
+    calcularCuota(precioTotal,cantCuotas)
+
+    seguir = prompt("Desea ingresar otro monto ? SI NO").toUpperCase();
+
+}while (seguir !== "NO");
